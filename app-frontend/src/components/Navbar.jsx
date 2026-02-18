caac1f458eba2e5ff23194e24aba1660e7cc8ba8
Gilbert Isaac Salazar <salazarg435@gmail.com>
2026-02-18 11:50:40 -0600

    Carousel working

--- app-frontend/src/components/Navbar.jsx
+++ app-frontend/src/components/Navbar.jsx
@@ -2,20 +2,40 @@ import { NavLink } from 'react-router-dom';
 function Navbar() {
 
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
                         <NavLink
                             className="nav-link"
                             to='/'
                             end
                         >
                         Home
+                        </NavLink>
+                        <NavLink
+                            className='nav-link'
+                            to='/'
+                            end
+                        >
+                            Reserve a Computer
+                        </NavLink>
+                        <NavLink 
+                            className='nav-link' 
+                            to='/' 
+                            end
+                        >
+                            About Us
+                        </NavLink>
+                        <NavLink
+                        className='nav-link'
+                        to='/'
+                        end>
+                            Login
                         </NavLink>
                     </div>
                 </div>
