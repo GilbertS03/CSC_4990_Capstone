4fdf678caeb39ea8c0e48733c5df62cab2795126
Christopher Maldonado <149415670+chrismald0228@users.noreply.github.com>
2026-03-02 11:14:24 -0600

    added cors midlleware

--- app-backend/api/main.py
+++ app-backend/api/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes import users, devices
 from .auth.routes import auth_router
+
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_method=["*"],
+    allow_headers=["*"]
+)
+
 
 app.include_router(users.router)
 app.include_router(devices.router)
