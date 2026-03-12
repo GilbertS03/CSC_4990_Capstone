b477d9922652ffec31384a62e708f2b18151225a
Alejandro Niño <alejandronino999@gmail.com>
2026-03-12 13:11:35 -0500

    get all users test

--- app-backend/pytest/UserServices.py
+++ app-backend/pytest/UserServices.py
@@ -4,16 +4,32 @@ from unittest.mock import Mock
 from api.services.users import *
 
 class TestServices(unittest.TestCase):
-    values = {"UserID": 1, "firstName": "Gilbert", "lastName": "Salazar", 
+    valuesUser = {"UserID": 1, "firstName": "Gilbert", "lastName": "Salazar", 
               "email": "test@gmail.com", 
               "password": "$2b$12$KyhGkEPsS4WWGtDdp/zI/upCPCqG4P535QgYs2Hh1NFq6YZlt6HMe",
               "weeklyHoursRemaining": 6.5, "roleId": 1, "role": "admin"}
-
-    def test_userFetchId_getExistingUserInfo_ReturnInfo(self):
-        mockUser = Mock(**self.values)
+    
+    valuesAllUsers = {{"UserID": 1, "firstName": "Gilbert", "lastName": "Salazar", 
+              "email": "test@gmail.com", 
+              "password": "$2b$12$KyhGkEPsS4WWGtDdp/zI/upCPCqG4P535QgYs2Hh1NFq6YZlt6HMe",
+              "weeklyHoursRemaining": 3, "roleId": 2, "role": "student"}, {"UserID": 2, "firstName": "Chris", "lastName": "Maldonado", 
+              "email": "test2@gmail.com", 
+              "password": "$2b$12$KyhGkEPsS4WWGtDdp/zI/suwYTw4P535QgYs2HhfghGs73Fs6HMe",
+              "weeklyHoursRemaining": 9, "roleId": 5, "role": "admin"}}
+    
+    def test_fetchUser_getAllUsers_returnInfo(self):
+        mockUser = Mock(**self.valuesAllUsers)
         mock_session = Mock()
         mock_session.exec.return_value.first.return_value = mockUser
         result = fetch_users_by_id(1, mock_session)
+        print("Result:", result)
+        assert result is not None
+
+    def test_userFetchId_getExistingUserInfo_returnInfo(self):
+        mockUser = Mock(**self.valuesUser)
+        mock_session = Mock()
+        mock_session.exec.return_value.first.return_value = mockUser
+        result = fetch_users(1, mock_session)
         print("Result:", result)
         assert result is not None
         assert result.email == "test@gmail.com"
@@ -21,7 +37,7 @@ class TestServices(unittest.TestCase):
         mock_session.exec.assert_called()
 
         
-    def test_userFetchId_getNonExistingUserInfo_ReturnInfo(self):
+    def test_userFetchId_getNonExistingUserInfo_returnNothing(self):
         mock_session = Mock()
         mock_session.exec.return_value.first.return_value = None
         result = fetch_users_by_id(0, mock_session)
