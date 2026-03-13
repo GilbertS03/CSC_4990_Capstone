81875f1de22fc6fb111fac89897d9c62763cd069
AlejandroN176 <147194662+AlejandroN176@users.noreply.github.com>
2026-03-13 16:46:05 -0500

    finished making all test cases for Devices endpoints, renamed files to match naming conventions and changed github action file for endpoint testing to happen all in one execution

--- app-backend/pytest/user-services.py
+++ app-backend/pytest/user-services.py
@@ -4,7 +4,7 @@ from unittest.mock import Mock
 from api.schema.user_schema import UserCreate
 from api.services.users import *
 
-class TestServices(unittest.TestCase):
+class TestUserServices(unittest.TestCase):
     UserValues = {"UserID": 1, "firstName": "Gilbert", "lastName": "Salazar", 
               "email": "test@gmail.com", 
               "password": "$2b$12$KyhGkEPsS4WWGtDdp/zI/upCPCqG4P535QgYs2Hh1NFq6YZlt6HMe",
@@ -20,96 +20,96 @@ class TestServices(unittest.TestCase):
               "weeklyHoursRemaining": 9, "roleId": 5, "role": "admin"}]
     
     def test_fetchUser_fetchExistingUsers_returnInfo(self):
-        mockUsers = [Mock(**user_data) for user_data in self.allUsersValues]
-        mock_session = Mock()
-        mock_session.exec.return_value.all.return_value = mockUsers
-        result = fetch_users(mock_session)
-        print("getAllUsers_returnInfo Result:", result[1])
+        mockUsers = [Mock(**userData) for userData in self.allUsersValues]
+        mockSession = Mock()
+        mockSession.exec.return_value.all.return_value = mockUsers
+        result = fetch_users(mockSession)
+        print("getAllUsers_returnInfo Result:", result)
         assert result is not None
-        mock_session.exec.assert_called()
+        mockSession.exec.assert_called()
 
     def test_fetchUser_fetchemptyUsers_returnNone(self):
-        mock_session = Mock()
-        mock_session.exec.return_value.all.return_value = []
-        result = fetch_users(mock_session)
+        mockSession = Mock()
+        mockSession.exec.return_value.all.return_value = []
+        result = fetch_users(mockSession)
         print("getemptyUsers_returnNone Result:", result)
         assert result == []
-        mock_session.exec.assert_called()
+        mockSession.exec.assert_called()
         
 
     def test_fetchUserRole_fetchExistingUserRole_returnRole(self):
         mockUser = Mock(**self.UserValues)
-        mock_session = Mock()
-        mock_session.exec.return_value.first.return_value = mockUser
-        result = fetch_user_role(mock_session, "test@gmail.com")
+        mockSession = Mock()
+        mockSession.exec.return_value.first.return_value = mockUser
+        result = fetch_user_role(mockSession, "test@gmail.com")
         print("fetchUserRole_returnRole Result:", result.role)
         assert result is not None
         assert result.role == "admin"
-        mock_session.exec.assert_called()
+        mockSession.exec.assert_called()
 
     def test_fetchUserRole_fetchNonExistingUserRole_returnNone(self):
-        mock_session = Mock()
-        mock_session.exec.return_value.first.return_value = None
-        result = fetch_user_role(mock_session, "test@gmail.com")
+        mockSession = Mock()
+        mockSession.exec.return_value.first.return_value = None
+        result = fetch_user_role(mockSession, "test@gmail.com")
         print("fetchUserRole_returnRole Result:", result)
         assert result is None
-        mock_session.exec.assert_called()
+        mockSession.exec.assert_called()
 
     def test_userFetchId_fetchExistingUserId_returnInfo(self):
         userID= 1
         mockUser = Mock(**self.UserValues)
-        mock_session = Mock()
-        mock_session.exec.return_value.first.return_value = mockUser
-        result = fetch_users_by_id(userID, mock_session)
+        mockSession = Mock()
+        mockSession.exec.return_value.first.return_value = mockUser
+        result = fetch_users_by_id(userID, mockSession)
         print("getExistingUserInfo_returnInfo Result:", result)
         assert result is not None
         assert result.email == "test@gmail.com"
         assert result.firstName == "Gilbert"
-        mock_session.exec.assert_called()
+        mockSession.exec.assert_called()
 
         
     def test_userFetchId_fetchNonExistingUserId_returnNothing(self):
         userId = 0
-        mock_session = Mock()
-        mock_session.exec.return_value.first.return_value = None
-        result = fetch_users_by_id(userId, mock_session)
+        mockSession = Mock()
+        mockSession.exec.return_value.first.return_value = None
+        result = fetch_users_by_id(userId, mockSession)
         print("getNonExistingUserInfo_returnNothing Result:", result)
         assert result is None
-        mock_session.exec.assert_called()
+        mockSession.exec.assert_called()
 
     def test_userCreate_createUserInfo_returnNewUser(self):
         newUser = UserCreate(email ="test@gmail.com",firstName= "Niko",lastName= "Ruiz",password= "123CookieMonster")
-        mock_session = Mock()
-        result = create_user(newUser,mock_session)
+        mockSession = Mock()
+        result = create_user(newUser,mockSession)
         print("createUserInfo_returnInfo Result:", result)
         assert result is not None
         assert result.email == "test@gmail.com"
         assert result.firstName == "Niko"
 
     def test_userCreate_createEmptyUserInfo_returnError(self):
-        mock_session = Mock()
+        mockSession = Mock()
         with self.assertRaises(Exception):
             newUser = UserCreate()
-            create_user(newUser, mock_session)
-        mock_session.add.assert_not_called()
-        mock_session.commit.assert_not_called()
+            create_user(newUser, mockSession)
+        mockSession.add.assert_not_called()
+        mockSession.commit.assert_not_called()
 
     def test_fetchUserByEmail_fetchExistingEmail_returnInfo(self):
         userEmail= "test@gmail.com"
         mockUser = Mock(**self.UserValues)
-        mock_session = Mock()
-        mock_session.exec.return_value.first.return_value = mockUser
-        result = fetch_user_by_email( mock_session,userEmail)
+        mockSession = Mock()
+        mockSession.exec.return_value.first.return_value = mockUser
+        result = fetch_user_by_email( mockSession,userEmail)
         print("FetchExistingEmail_returnInfo result: " , result.firstName)
         assert result is not None
         assert result.firstName == "Gilbert"
-        mock_session.exec.assert_called()
+        mockSession.exec.assert_called()
 
     def test_fetchUserByEmail_fetchNonExistingEmail_returnNothing(self):
         userEmail= "test@gmail.com"
-        mock_session = Mock()
-        mock_session.exec.return_value.first.return_value = None
-        result = fetch_user_by_email( mock_session,userEmail)
+        mockSession = Mock()
+        mockSession.exec.return_value.first.return_value = None
+        result = fetch_user_by_email( mockSession,userEmail)
         print("FetchNonExistingEmail_returnNone result: " , result)
         assert result is None
-        mock_session.exec.assert_called()
+        mockSession.exec.assert_called()
