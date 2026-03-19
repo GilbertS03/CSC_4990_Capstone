8e90caf2b8c3e6024a2a0c52271b5895d6f2dc66
AlejandroN176 <147194662+AlejandroN176@users.noreply.github.com>
2026-03-19 12:30:28 -0500

    pushing for sprint review leaving out reservations for now

--- app-backend/pytest/reservation-services.py
+++ app-backend/pytest/reservation-services.py
@@ -15,7 +15,6 @@ class TestReservationServices(unittest.TestCase):
         mockSession = Mock()
         mockSession.exec.return_value.all.return_value = mockReservations
         result = fetch_all_reservations(mockSession)
-        print("fetchAllReservations_returnInfo Result:", result[1].reservationId)
         assert result is not None
         assert result[1].reservationId == 2
         mockSession.exec.assert_called()
@@ -37,13 +36,14 @@ class TestReservationServices(unittest.TestCase):
         assert len(result) == 2
         mockSession.exec.assert_called()
 
-        ##FilterConstraints might not be working correctly here
+        ##Fetchdevicestatuses inputs is not working properly
+        ##TODO boundary test when fixed
     def test_fetchDeviceStatus_userid_fetchExisitingReservation_returnInfo(self):
         mockReservations = [Mock(**data) for data in self.allReservationValues]
         mockSession = Mock()
         mockSession.exec.return_value.all.return_value = mockReservations
-        result = fetch_reservation_statuses(mockSession, 1,)
-        print("userid_fetchReservationStatuses_returnInfo Result:", result)
+        result = fetch_reservation_statuses(mockSession, 100,40, "canceled")
+        print("userid_fetchReservationStatuses_returnInfo Result:", len(result))
         assert result is not None
         assert result[1].reservationId == 2
         assert len(result) == 2
@@ -53,6 +53,7 @@ class TestReservationServices(unittest.TestCase):
         mockSession = Mock()
         mockSession.exec.return_value.all.return_value = []
         result = fetch_reservation_statuses(mockSession)
-        print("fetchReservationStatuses_returnEmpty Result:", result)
         assert result == []
         mockSession.exec.assert_called()    
+    
+    def test_CreateReservation_
