8767b8beb7adf3c54bc2beb64527f1a890bdb854
ShinobiNikko <ruiznicolas400@gmail.com>
2026-03-26 13:41:42 -0500

    added room endpoint, and route, and schema change

--- app-backend/api/routes/rooms.py
+++ app-backend/api/routes/rooms.py
@@ -24,6 +24,14 @@ def get_room_layouts(session: SessionDep, limit: int = 100):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error retrieving room layouts: {e}")
     
+@router.get("/room-layouts/{roomId}", response_model=RoomLayout)
+def get_room_layout_by_roomId(roomId: int, session: SessionDep):
+    try:
+        roomLayout= fetch_room_layouts_by_id(roomId, session)
+        return roomLayout
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error retrieving room layout: {e}")
+    
 @router.get("/{buildingId}", response_model=list[RoomPublic])
 def get_rooms_by_buildingId(buildingId: int, session: SessionDep):
     try:
