ff5ef65a8943e87a64b1fca98735f864d525dc9c
Christopher Maldonado <149415670+chrismald0228@users.noreply.github.com>
2026-04-01 11:42:40 -0500

    SQLalchemy couldnt process timedelta, fixed Building model to accurately convert to timedelta

--- app-backend/api/models/Buildings.py
+++ app-backend/api/models/Buildings.py
@@ -1,10 +1,11 @@
 from sqlmodel import SQLModel, Field
+from sqlalchemy import Column, Time
 from datetime import timedelta
 
 class Buildings(SQLModel, table=True):
     __tablename__ = "Buildings"
 
     buildingId: int = Field(default=None, primary_key=True)
-    openTime: timedelta
-    closeTime: timedelta
+    openTime: timedelta = Field(sa_column=Column(Time))
+    closeTime: timedelta = Field(sa_column=Column(Time))
     buildingName: str 
