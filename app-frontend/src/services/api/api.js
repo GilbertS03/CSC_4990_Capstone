f4d490c24d9f4b900aabeb975a36ce51a36789c0
AlejandroN176 <147194662+AlejandroN176@users.noreply.github.com>
2026-04-14 19:48:23 -0500

    made the api.js config ready for deployment

--- app-frontend/src/services/api/api.js
+++ app-frontend/src/services/api/api.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
+import {SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
 
-const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL
-});
 
-export default api;
+async function createAPI(){
+    if(import.meta.env.VITE_ENV === 'dev'){
+        const api = axios.create({baseURL: import.meta.env.VITE_API_BASE_URL});
+        return api;
+    } else {
+        const client = new SecretsManagerClient({region: 'us-east-1'});
+        const {SecretString} =await client.send(new GetSecretValueCommand({SecretId:"myapp/front-end"}));
+        const api = axios.create({baseURL: SecretString});
+        return api;
+    }
+
+}
+
+export default await createAPI();
