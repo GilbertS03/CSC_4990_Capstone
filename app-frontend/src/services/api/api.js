20942012b0dba6c5a0a7508e55e33d1678ba4f71
AlejandroN176 <147194662+AlejandroN176@users.noreply.github.com>
2026-04-21 11:06:51 -0500

    deleted unused images and took out dependency call and uninstalled dependency

--- app-frontend/src/services/api/api.js
+++ app-frontend/src/services/api/api.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
-import {SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
-
 
 async function createAPI(){
     if(import.meta.env.VITE_ENV === 'dev'){
