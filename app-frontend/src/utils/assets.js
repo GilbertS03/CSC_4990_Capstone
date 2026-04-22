ddb602b39a2284a347f8e3948269597accb31abe
Alejandro Nino <147194662+AlejandroN176@users.noreply.github.com>
2026-04-21 19:10:44 -0500

    Update assets.js
    meant to have /assets

--- app-frontend/src/utils/assets.js
+++ app-frontend/src/utils/assets.js
@@ -1,5 +1,5 @@
 const S3_URL = import.meta.env.VITE_ENV === 'dev'
   ? 'http://localhost:5173/assets'
-  : 'https://dvlybc03nsv3j.cloudfront.net';
+  : 'https://dvlybc03nsv3j.cloudfront.net/assets';
 
 export const asset = (path) => `${S3_URL}/${path}`;
