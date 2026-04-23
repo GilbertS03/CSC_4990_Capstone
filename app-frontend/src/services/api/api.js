a7a287921bf153e3e5fe046239ddd9012d1eef66
Alejandro Nino <147194662+AlejandroN176@users.noreply.github.com>
2026-04-23 01:44:39 -0500

    Update api.js
    removed slash

--- app-frontend/src/services/api/api.js
+++ app-frontend/src/services/api/api.js
@@ -5,7 +5,7 @@ async function createAPI(){
         const api = axios.create({baseURL: "http://127.0.0.1:8000"});
         return api;
     } else{
-        const api = axios.create({baseURL: "http://34.202.2.180:8000/"});
+        const api = axios.create({baseURL: "http://34.202.2.180:8000"});
         return api;
     }
 
